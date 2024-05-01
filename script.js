aa03500c460db77a1e686946307ff5b5dad9f757
Alireza Zabet <ahzabet23@gmail.com>
2024-05-01 20:36:50 +0400

    Add explicit type annotation for canvas rendering context

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const canvas = document.getElementById("canvas");
+/** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d");
 
 ctx.drawImage(document.getElementById("source"), 0, 0);
