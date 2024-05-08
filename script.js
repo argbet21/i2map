088051b98ac548d99fce5eadbe2334f50c5b858d
Alireza Zabet <ahzabet23@gmail.com>
2024-05-08 12:58:56 +0400

    Implement app initialization

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ import countries from "./modules/countries/countries.js";
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.drawImage(document.getElementById("source"), 0, 0);
+init();
 
-drawOrigin(ctx, countries.brazil.x, countries.brazil.y);
-drawDestination(ctx, countries.iran.x, countries.iran.y);
-drawQuadraticBezierRoute(
-  ctx,
-  countries.brazil.x,
-  countries.brazil.y,
-  countries.iran.x,
-  countries.iran.y
-);
+function init() {
+  ctx.drawImage(document.getElementById("source"), 0, 0);
+  drawOrigin(ctx, countries.brazil.x, countries.brazil.y);
+  drawDestination(ctx, countries.iran.x, countries.iran.y);
+  drawQuadraticBezierRoute(
+    ctx,
+    countries.brazil.x,
+    countries.brazil.y,
+    countries.iran.x,
+    countries.iran.y
+  );
+}
